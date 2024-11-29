4a1ef12c442b2a4e19c2bd0aa78806c3767e6caa
GeIF350 <d.geovana@estudante.ifmt.edu.br>
2024-11-29 10:37:30 -0400

    jogo desenvolvido com sucesso!

---
+++ app.js
@@ -0,0 +1,28 @@
+let tentativas=0;
+let nome = prompt("Primeiro, como devo te chamar?");
+let limite = prompt("Devo sortear um número entre 0 e quanto?")
+let numero;
+let numeroSorteado = parseInt(Math.random()*limite);
+console.log(numeroSorteado);
+
+alert(`O número está entre 0 e ${limite}`);
+
+while(numero != numeroSorteado){
+    tentativas++
+    numero = prompt("Digite uma tentativa: ");
+    if(numero == numeroSorteado) {
+        if(tentativas==1){
+            alert(`Parabéns! Você acertou o número ${numeroSorteado} com ${tentativas} tentativa.`)
+            break;
+        } else {
+            alert(`Parabéns! Você acertou o número ${numeroSorteado} com ${tentativas} tentativas.`)
+            break;
+        }
+    } else {
+        if(numero < numeroSorteado){
+            alert(`O número secreto é maior que ${numero}`)
+        } else {
+            alert(`O número secreto é menor que ${numero}`)
+        }
+    }
+}
