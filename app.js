c17dabc74a2f7f436fe27fe8b8ae547afdcc25b0
GeIF350 <d.geovana@estudante.ifmt.edu.br>
2024-11-30 10:46:45 -0400

    apenas uma correção de sintaxe

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let tentativas=0;
 let nome = prompt("Primeiro, como devo te chamar?");
-let limite = prompt("Devo sortear um número entre 0 e quanto?")
+let limite = prompt("Devo sortear um número entre 0 e quanto?");
 let numero;
 let numeroSorteado = parseInt(Math.random()*limite);
 // console.log(numeroSorteado);
@@ -8,7 +8,7 @@ let numeroSorteado = parseInt(Math.random()*limite);
 alert(`O número está entre 0 e ${limite}`);
 
 while(numero != numeroSorteado){
-    tentativas++
+    tentativas++;
     numero = prompt("Digite uma tentativa: ");
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
     if(numero == numeroSorteado) {
@@ -16,9 +16,9 @@ while(numero != numeroSorteado){
         break;
     } else {
         if(numero < numeroSorteado){
-            alert(`O número secreto é maior que ${numero}`)
+            alert(`O número secreto é maior que ${numero}`);
         } else {
-            alert(`O número secreto é menor que ${numero}`)
+            alert(`O número secreto é menor que ${numero}`);
         }
     }
 }
